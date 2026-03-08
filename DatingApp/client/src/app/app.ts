43aa43c1716381ccb730e25bc9b89d8755e22202
Manish <Manish@DESKTOP-DGSRLA4>
2026-03-08 20:59:55 +0530

    section 5- added home page

--- DatingApp/client/src/app/app.ts
+++ DatingApp/client/src/app/app.ts
@@ -3,16 +3,20 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { JsonPipe } from '@angular/common';
+import { Home } from "../features/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [Nav],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
 
   private http = inject(HttpClient);
+  private accountService = inject(AccountService);
   protected  title = 'Dating App';
   protected members = signal<any>([]);
 
@@ -31,6 +35,15 @@ export class App implements OnInit {
     // });
 
       this.members.set(await this.getMembers());
+      this.setCurrentUser();
+  }
+
+  setCurrentUser()
+  {
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 
   async getMembers()
