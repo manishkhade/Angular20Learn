3682041803d764ac0ac16c5874fd8a82d6a18f62
Manish <Manish@DESKTOP-DGSRLA4>
2026-03-10 21:23:34 +0530

    section 6 - adding route guard

--- DatingApp/client/src/app/app.routes.ts
+++ DatingApp/client/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { MemberList } from '../features/members/member-list/member-list';
 import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
+import { authGuard } from '../core/guards/auth-guard';
 
 export const routes: Routes = [
     {
@@ -12,7 +13,8 @@ export const routes: Routes = [
     },
     {
         path: 'members',
-        component: MemberList
+        component: MemberList,
+        canActivate: [authGuard]
     },
     {
         path: 'members/:id',
